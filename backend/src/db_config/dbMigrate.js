4bed414dea07a60c35c54943fb1cbd2e71c9ffd5
Kavan <kavankaverappa5@gmail.com>
2025-03-05 22:38:56 +0530

    tabel setup in mysql from code with help of connection

---
+++ backend/src/db_config/dbMigrate.js
@@ -0,0 +1,43 @@
+const pool = require('./db.js');
+
+export const migrate =async()=>{
+    const connection = await pool.getConnection();
+    try {
+        //type of vehicles table
+        await connection.query(
+            `CREATE TABLE IF NOT EXISTS vehicle_type(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            wheels INT NOT NULL
+            )`
+        )
+        //vehicle table
+        await connection.query(
+            `CREATE TABLE IF NOT EXISTS vehicle(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            type_id INT,
+            model VARCHAR(100) NOT NULL,
+            FORIEGN KEY(type_id) REFERENCES vehicle_type(id)
+            )`
+        )
+        //booking table
+        await connection.query(`
+            CREATE TABLE IF NOT EXISTS booking(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
+            vehicle_id INT NOT NULL,
+            start_date DATE NOT NULL,
+            end_date DATE NOT NULL,
+            FORIEGN KEY(vehicle_id) REFERENCES vehicle(id)
+            )
+            `)
+        console.log('Migration successfull');
+    } catch (error) {
+        console.log("Migration error: ", error);
+        
+    }
+    finally{
+        connection.release();
+    }
+}
