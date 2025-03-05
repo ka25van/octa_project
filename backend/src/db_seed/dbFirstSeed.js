ca36727410800498b95eb156d4cfd67906815e2a
Kavan <kavankaverappa5@gmail.com>
2025-03-05 22:39:43 +0530

    Added initial set of data as per the table setup

---
+++ backend/src/db_seed/dbFirstSeed.js
@@ -0,0 +1,37 @@
+const pool = require('./db.js');
+
+export const firseed = async () => {
+    const connection = await pool.getConnection();
+    try {
+        //adding vehicle type
+        const [vehicleType] = await connection.query('INSERT INTO vehicle_type(name,wheels)?',
+            [[
+                ['Hatchback', 4],
+                ['SUV', 4],
+                ['Sedan', 4],
+                ['Cruiser', 2],
+                ['Sports', 2]
+
+            ]]
+        );
+        const vehicleTypeId = vehicleType.insertId;
+
+        //adding vehicles
+        await connection.query('INSERT INTO vehicle (type_id, model) VALUES ?',
+            [[
+                [vehicleTypeId, 'Swift Hatchback'],
+                [vehicleTypeId + 1, 'Toyota Fortuner'],
+                [vehicleTypeId + 2, 'Toyata Camrey'],
+                [vehicleTypeId + 3, 'Classic 350'],
+                [vehicleTypeId + 4, 'Ninja H2R']
+            ]]
+        );
+        console.log('Database seeded successfully');
+
+    } catch (error) {
+        console.log("Seeding Error: ", error);
+
+    } finally {
+        connection.release()
+    }
+}
